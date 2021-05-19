c062f5695713c1afd27238c2159ef75e962ba21a
Aurora Softwares <73100596+AuroraSoftwares@users.noreply.github.com>
2021-05-20 00:14:59 +0530

    Update Base64.php

--- v1.0/Cherry/Utils/Base64.php
+++ v1.0/Cherry/Utils/Base64.php
@@ -6,8 +6,8 @@
 
 	/**
 	 * <b>FILE : </b>Base64.php<br>
-     * <b>COPYRIGHT : </b>©2020 | AuroraSoftwares<br>
-     * <b>VERSION : </b>1.0
+     	 * <b>COPYRIGHT : </b>Â©2021 | Aurora Softwares<br>
+     	 * <b>VERSION : </b>1.0
 	 */
 	class Base64
 	{
